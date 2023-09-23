4c2f34ad3f163f013fb341d73f6da2785a8fca04
Sohan Ahmed <sohan.ahmedself@gmail.com>
2023-09-23 19:41:34 +0600

    first commit

---
+++ ProductManagementApp/Controllers/ProductInventories.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementApp.Models;
+using ProductManagementApp.Repositories.Interfaces;
+
+namespace ProductManagementApp.Controllers
+{
+    public class ProductInventories : Controller
+    {
+        private readonly IWebHostEnvironment env;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IGenericRepo<ProductInventory> repo;
+        public ProductInventories(IUnitOfWork unitOfWork, IWebHostEnvironment env)
+        {
+            this.unitOfWork = unitOfWork;
+            this.repo = this.unitOfWork.GetRepo<ProductInventory>();
+            this.env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var data = await this.repo.GetByIdAsync(x => x.CompanyId == id);
+            ViewBag.Companies = await this.repo.ExecuteSqlCollection<Company>("SELECT * FROM Companies");
+            return View(data);
+        }
+    }
+}
